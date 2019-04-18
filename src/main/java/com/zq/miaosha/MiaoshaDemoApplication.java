4a9e28ce88024eaa2e7519e141998b6f79408580
zengqiang <1092829162@qq.com>
2019-04-18 23:32:44 +0800

    第一次提交

---
+++ src/main/java/com/zq/miaosha/MiaoshaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zq.miaosha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiaoshaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiaoshaDemoApplication.class, args);
+    }
+
+}
